00e138d076a8d31d4f7d28a22056b03d8f948d41
KRYPTONITE\k.zamotin <k.zamotin@kryptonite.ru>
2024-08-06 10:36:25 +0300

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  
+  var st = []
+  
+  for (let i=0;i<str.length;i++)
+  {
+      var br = str.at(i);
+      console.log("br",br)
+      for (let j in bracketsConfig) {
+          
+          if ((bracketsConfig[j].at(1) === br) ){
+                  if(st.length >0){
+                      console.log("pop", bracketsConfig[j])
+                      var r = st.pop()
+                      if (r !== bracketsConfig[j].at(0)){
+                          st.push(r)
+                          st.push(br)
+                      }
+                      console.log(r)
+                  }
+                  else {
+                      console.log("push", bracketsConfig[j])
+                       st.push(br)
+                  }
+                  
+          }
+          else {
+              if (bracketsConfig[j].at(0) === br){
+                  console.log("push", bracketsConfig[j])
+                  st.push(br)
+          }
+         }
+      }
+  }
+  console.log(st)
+  return st.length>0? false: true
 }
